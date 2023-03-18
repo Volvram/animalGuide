88a19d788a60d6047097b535d3c261996c2b2c03
Julia <yuliabobrovich@yandex.ru>
2023-03-18 13:22:25 +0300

    Fixed categories image and added some articles

--- src/db/articles.ts
+++ src/db/articles.ts
@@ -20,4 +20,19 @@ export const articles: ArticleType[] = [
         image: "https://faunistics.com/wp-content/uploads/2019/02/4-735x551.jpg",
         text: "Относится к редкому подвиду кошачьих, который смог дожить до наших дней"
     },
+    {
+        id: "4",
+        image: "https://faunistics.com/wp-content/uploads/2019/02/5-735x490.jpg",
+        text: "Обыкновенный таймень так же занесен в Красную Книгу и находится под охраной во многих регионах России"
+    },
+    {
+        id: "5",
+        image: "https://faunistics.com/wp-content/uploads/2019/02/6-735x539.jpg",
+        text: "Кабарга является представителем парнокопытных и по внешнему виду напоминает оленя"
+    },
+    {
+        id: "6",
+        image: "https://faunistics.com/wp-content/uploads/2019/02/7-735x528.jpg",
+        text: "Это интересное и не большое животное, которое находится под охраной в определенных регионах России"
+    }
 ]
--- src/pages/Main/components/Categories/Categories.tsx
+++ src/pages/Main/components/Categories/Categories.tsx
@@ -1,18 +1,49 @@
 import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet, View, Text, Image, ImageBackground } from "react-native";
 import { COLORS, SIZES } from "../../../../config/constants";
 
 const Categories = () => {
+    const [imageSize, setImageSize] = React.useState({width: 294, height: 137});
+
+    React.useEffect(() => {
+        try {
+            const imageSource = require('../../../../../assets/animalCategory.png');
+        
+            const resolvedImageSource = Image.resolveAssetSource(imageSource);
+
+            setImageSize({
+                width: resolvedImageSource.width,
+                height: resolvedImageSource.height
+            })
+        } catch(e) {
+
+        }
+        
+    }, []);
 
     return (
         <View style={styles.main_categories}>
-            <Image style={styles.main_categories_image} source={require('../../../../../assets/animalCategory.png')} resizeMode="contain"/>
-            <View style={styles.main_categories_background}>
-                <View style={{width: 7, height: 19, backgroundColor: COLORS.yellow}} />
-                <Text style={{marginTop: 130, marginBottom: 30, marginLeft: 22, color: COLORS.white, fontSize: SIZES.h3, fontWeight: '700', }}>
-                    Животные
-                </Text>
-            </View>
+            <ImageBackground style={{aspectRatio: imageSize.width / imageSize.height, zIndex: 0}} 
+            source={require('../../../../../assets/animalCategory.png')} resizeMode="contain">
+                <LinearGradient
+                    colors={[
+                    'rgba(30, 31, 24, 0)',
+                    'rgba(30, 31, 24, 0)',
+                    'rgba(36, 37, 30, 0.77)',
+                    '#25261F',
+                    ]}
+                    start={[0, 0.9]}
+                    style={{flex: 1, justifyContent: 'center', borderRadius: 30}}>
+
+                    <View style={{aspectRatio: imageSize.width / imageSize.height, ...styles.main_categories_background}}>
+                        <View style={{width: 7, height: 19, backgroundColor: COLORS.yellow}} />
+                        <Text style={{aspectRatio: imageSize.width / imageSize.height, ...styles.main_categories_background_text}}>
+                            Животные
+                        </Text>
+                    </View>
+                </LinearGradient>
+            </ImageBackground>
         </View>
     )
 }
@@ -21,22 +52,19 @@ const styles = StyleSheet.create({
     main_categories: {
         marginTop: 30,
         width: '100%',
-        height: 200,
     },
-
-    main_categories_image: {
-        width: '100%',
-        height: 200,
-        zIndex: 0,
-    },
-
     main_categories_background: {
-        position: 'absolute',
         width: '100%',
-        height: 200,
-        backgroundImage: 'linear-gradient(0deg, #25261F 15.38%, rgba(36, 37, 30, 0.77) 42.83%, rgba(30, 31, 24, 0) 70.77%)',
         borderRadius: 30,
         zIndex: 1,
+    },
+    main_categories_background_text: {
+        marginTop: "30%", 
+        marginBottom: 30, 
+        marginLeft: 22, 
+        color: COLORS.white, 
+        fontSize: SIZES.h3, 
+        fontWeight: '700',
     }
 });
 
